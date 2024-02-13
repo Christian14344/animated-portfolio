dc4b53100a58c2527fb13a936b155701ea1456c4
Christian Baclea-an <christianbacleaan@gmail.com>
2024-02-12 21:17:11 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import "./app.scss";
+import Navbar from "./component/navbar/Navbar";
+import Hero from "./component/hero/Hero";
+import Parallax from "./component/parallax/Parallax";
+import Test from "./Test";
+import Services from "./component/services/Sarvices";
+import Portfolio from "./component/portfolio/Portfolio";
+import Contact from "./component/contact/Contact";
+import Cursor from "./component/cursor/Cursor";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <Cursor/>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+    </section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section><Services/></section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <Portfolio/>
+    <section id="Contact">
+    <Contact/>
+    </section>
+    {/* <Test/>
+    <Test/> */}
+    
+  </div>;
 };
 
 export default App;
